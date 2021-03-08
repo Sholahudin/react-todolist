74d61254fb44832930e00e71e701b8b65ee5c1cb
Sholahudin <79613788+Sholahudin@users.noreply.github.com>
2021-03-09 05:31:55 +0700

    menambah useState di Todolist

--- src/pages/Todolist.jsx
+++ src/pages/Todolist.jsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 
 import Paper from "../components/Paper";
 import Header from "../components/Header";
@@ -6,6 +6,12 @@ import TodoForm from "../components/TodoForm";
 import Todos from "../components/Todos";
 
 const Todolist = () => {
+  const [todos, setTodos] = useState([
+    { text: "Learning React!" },
+    { text: "Learning React Hook!" },
+    { text: "Learning styling in React" }
+  ]);
+
   return (
     <Paper>
       <Header />
