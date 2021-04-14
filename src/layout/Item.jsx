a246a2209062c4027bcb77ea442700c04501b032
Sholahudin <79613788+Sholahudin@users.noreply.github.com>
2021-04-14 17:30:48 +0700

    menyelesaikan komponens Item

---
+++ src/layout/Item.jsx
@@ -0,0 +1,26 @@
+/** @jsxImportSource @emotion/react */
+import { css } from "@emotion/react";
+import PropTypes from "prop-types";
+
+const Item = ({ children, flex }) => {
+  return (
+    <div
+      className="flex-item"
+      css={css`
+        flex: ${flex};
+      `}
+    >
+      {children}
+    </div>
+  );
+};
+
+Item.propTypes = {
+  children: PropTypes.oneOf([
+    PropTypes.arrayOf(PropTypes.node),
+    PropTypes.node
+  ]),
+  flex: PropTypes.oneOfType([PropTypes.number, PropTypes.string])
+};
+
+export default Item;
