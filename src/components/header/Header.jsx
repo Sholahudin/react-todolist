c451f24897acf9b61fa599a6464a385c606dcbd8
Sholahudin <79613788+Sholahudin@users.noreply.github.com>
2021-07-24 06:03:13 +0700

    memanggil button class di header

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -1,6 +1,6 @@
 /** @jsxImportSource @emotion/react */
 import PropTypes from "prop-types";
-import Button from "../button/ButtonNoJSX";
+import Button from "../button/ButtonClass";
 import { useTheme } from "@emotion/react";
 
 // import styles from "./header.module.css";
