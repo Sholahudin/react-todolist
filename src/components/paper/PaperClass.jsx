2166a1e2ea0a2efc745c281a2bd3189d41322a44
Sholahudin <79613788+Sholahudin@users.noreply.github.com>
2021-07-24 17:46:59 +0700

    menyelesaikan komponen paper class

--- src/components/paper/PaperClass.jsx
+++ src/components/paper/PaperClass.jsx
@@ -1,11 +1,31 @@
 import React from "react";
 import styled from "@emotion/styled";
-import { withTheme } from "@emotion/react";
+import { withTheme } from "emotion-theming";
 import PropTypes from "prop-types";
+
+const StyledPaper = styled.div`
+  width: 600px;
+  height: 800px;
+  background-color: ${(props) => props.theme.color.primary};
+  border-radius: 4px;
+
+  padding: 32px;
+`;
+
+const StyledFrame = styled.div`
+  border: 1px solid ${(props) => props.theme.color.primary.black};
+  height: 100%;
+`;
 
 class Paper extends React.Component {
   render() {
-    return <div />;
+    const { children, theme } = this.props;
+
+    return (
+      <StyledPaper theme={theme}>
+        <StyledFrame theme={theme}>{children}</StyledFrame>
+      </StyledPaper>
+    );
   }
   static propTypes = {
     children: PropTypes.oneOfType([
@@ -15,4 +35,4 @@ class Paper extends React.Component {
   };
 }
 
-export default Paper;
+export default withTheme(Paper);
