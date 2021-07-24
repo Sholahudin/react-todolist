5742b685dae887ff94b82dfd13793e8c815ac2d1
Sholahudin <79613788+Sholahudin@users.noreply.github.com>
2021-07-24 17:27:43 +0700

    latihan membuat class

--- src/components/paper/PaperClass.jsx
+++ src/components/paper/PaperClass.jsx
@@ -1,3 +1,18 @@
 import React from "react";
-import { withTheme } from "@emotion/styled";
+import styled from "@emotion/styled";
+import { withTheme } from "@emotion/react";
 import PropTypes from "prop-types";
+
+class Paper extends React.Component {
+  render() {
+    return <div />;
+  }
+  static propTypes = {
+    children: PropTypes.oneOfType([
+      PropTypes.arrayOf(PropTypes.node),
+      PropTypes.node
+    ])
+  };
+}
+
+export default Paper;
