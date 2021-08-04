174c304631887f24692487a77b9a66caec5a0d21
Sholahudin <79613788+Sholahudin@users.noreply.github.com>
2021-08-04 20:23:44 +0700

    menyelesaikan todoform class component

--- src/components/todoform/TodoFormClass.jsx
+++ src/components/todoform/TodoFormClass.jsx
@@ -7,6 +7,20 @@ import Container from "../../layout/Container";
 import Item from "../../layout/Item";
 import Button from "../button/Button";
 
+const StyledInput = styled.input`
+  background: unset;
+  border: unset;
+  padding: 0 64px;
+  width: 100%;
+
+  border-bottom: 1px solid ${(props) => props.theme.color.primary.red};
+  outline: unset;
+
+  font-family: "Homemade Apple", sans-serif;
+  font-size: 16px;
+  text-transform: lowercase;
+`;
+
 class TodoForm extends React.Component() {
   state = {
     value: ""
@@ -14,6 +28,7 @@ class TodoForm extends React.Component() {
 
   handleFormSubmit = (e) => {
     const { value } = this.state;
+    const { addTodo } = this.props;
 
     e.preventDefault();
 
@@ -24,18 +39,50 @@ class TodoForm extends React.Component() {
 
     if (value.length > 40) {
       alert("Please, create a shorter todo.");
-      setValue("");
+      this.setState({
+        value: ""
+      });
       return;
     }
 
     addTodo(this.state.value);
-    setValue("");
+    this.setState({
+      value: ""
+    });
+  };
+
+  handleOnChange = (e) => {
+    this.setState({
+      value: e.target.value
+    });
   };
 
   render() {
-    const { addTodo, showAdd } = this.props;
+    const { showAdd, theme } = this.props;
 
-    return <div></div>;
+    if (showAdd) {
+      return (
+        <section className="todoform-container">
+          <form onSubmit={this.handleFormSubmit}>
+            <Container alignItems="flex-start">
+              <Item flex="1" padding="0 0 0 16px">
+                <StyledInput
+                  type="text"
+                  value={this.value}
+                  theme={theme}
+                  onChange={this.handleOnChange}
+                />
+              </Item>
+              <Item>
+                <Button text="Add" />
+              </Item>
+            </Container>
+          </form>
+        </section>
+      );
+    } else {
+      return null;
+    }
   }
   static propTypes = {
     addTodo: PropTypes.func.isRequired,
