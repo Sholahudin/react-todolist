0a1c861e4c98ab71a6b502115f4f4849d38c2f3a
Sholahudin <79613788+Sholahudin@users.noreply.github.com>
2021-08-07 20:25:37 +0700

    pembuatan todosclass component

---
+++ src/components/todos/TodosClass.jsx
@@ -0,0 +1,25 @@
+import React from "react";
+import PropTypes from "prop-types";
+import styled from "@emotion/styled";
+import Container from "../../layout/Container";
+import Todo from "../todo/TodoClass";
+
+const StyledTodoPlaceHolderText = styled.div`
+  font-family: ${theme.font.primary}, sans-serif;
+  font-size: 24px;
+  text-align: center;
+  padding: 16px;
+`;
+
+const StyledTodoPlaceHolderTextAddbtn = styled.span`
+  font-family: ${theme.font.secondary}, sans-serif;
+  font-size: 24px;
+`;
+
+class Todos extends React.Component {
+  render() {
+    return;
+  }
+}
+
+export default Todos;
