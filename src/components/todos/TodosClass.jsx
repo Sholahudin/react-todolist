d953535f8d8ca46776a02a0073c17cee2287da34
Sholahudin <79613788+Sholahudin@users.noreply.github.com>
2021-08-08 08:01:58 +0700

    llll

--- src/components/todos/TodosClass.jsx
+++ src/components/todos/TodosClass.jsx
@@ -5,21 +5,32 @@ import Container from "../../layout/Container";
 import Todo from "../todo/TodoClass";
 
 const StyledTodoPlaceHolderText = styled.div`
-  font-family: ${theme.font.primary}, sans-serif;
+  font-family: ${(props) => props.theme.font.primary}, sans-serif;
   font-size: 24px;
   text-align: center;
   padding: 16px;
 `;
 
 const StyledTodoPlaceHolderTextAddbtn = styled.span`
-  font-family: ${theme.font.secondary}, sans-serif;
+  font-family: ${(props) => props.theme.font.secondary}, sans-serif;
   font-size: 24px;
 `;
 
 class Todos extends React.Component {
   render() {
+    const { todos, completeTodo } = this.props;
+
     return;
   }
+
+  static propTypes = {
+    todos: PropTypes.arrayOf(
+      PropTypes.shape({
+        text: PropTypes.string
+      })
+    ),
+    completeTodo: PropTypes.func.isRequired
+  };
 }
 
 export default Todos;
