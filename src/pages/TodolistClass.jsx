6291f221f2bbeed946a4f6898157434b69455d03
Sholahudin <79613788+Sholahudin@users.noreply.github.com>
2021-08-11 18:40:57 +0700

    membuat file todolist class

---
+++ src/pages/TodolistClass.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+
+import Paper from "../components/paper/PaperClass";
+import Header from "../components/header/HeaderClass";
+import TodoForm from "../components/todoform/TodoFormClass";
+import Todos from "../components/todos/TodosClass";
+
+import Container from "../layout/Container";
+
+class Todolist extends React.Component {
+  render() {
+    return;
+  }
+}
+
+export default Todolist;
