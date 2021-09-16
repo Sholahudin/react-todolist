7fec80da91720dcf183bdd35afc188c78ee27408
Sholahudin <79613788+Sholahudin@users.noreply.github.com>
2021-09-16 17:12:52 +0700

    finishing todolist class

--- src/pages/TodolistClass.jsx
+++ src/pages/TodolistClass.jsx
@@ -8,8 +8,72 @@ import Todos from "../components/todos/TodosClass";
 import Container from "../layout/Container";
 
 class Todolist extends React.Component {
+  state = {
+    todos: [
+      { text: "Learning React!", isCompleted: false },
+      { text: "Learning React Hook!", isCompleted: false },
+      { text: "Learning styling in React", isCompleted: false }
+    ],
+    showAdd: false
+  };
+
+  addTodo = (value) => {
+    const { todos } = this.state;
+    if (todos.length < 10) {
+      const addedTodo = [...todos, { text: value, isCompleted: false }];
+
+      this.setState({
+        todos: addedTodo
+      });
+    } else {
+      alert("No more Line Space, Sorry");
+    }
+  };
+
+  completeTodo = (index) => {
+    const { todos } = this.state;
+    const addedTodo = [...todos];
+    addedTodo[index].isCompleted = !addedTodo[index].isCompleted;
+
+    this.setState({
+      todos: addedTodo
+    });
+  };
+
+  clearTodos = () => {
+    if (this.state.showAdd) {
+      alert("Finish Type Before Clear");
+      return;
+    }
+
+    this.setState({
+      todos: []
+    });
+  };
+
+  showAddToggle = () => {
+    this.setState((prevState) => ({ showAdd: !prevState.showAdd }));
+  };
+
   render() {
-    return;
+    const { todos, showAdd } = this.state;
+    return (
+      <Paper>
+        <Container
+          flexDirection="column"
+          justifyContent="space-between"
+          height="100%"
+        >
+          <Header
+            showAddToggle={this.showAddToggle}
+            showAdd={showAdd}
+            clearTodos={this.clearTodos}
+          />
+          <TodoForm addTodo={this.addTodo} showAdd={showAdd} />
+          <Todos todos={todos} completeTodo={this.completeTodo} />
+        </Container>
+      </Paper>
+    );
   }
 }
 
